5e7351d7c49b6b7da9ee5d93340770e2473dc6fd
Somesh Vemula <ratansomesh@gmail.com>
2023-11-10 15:15:55 -0500

    Update the API key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "01021587a22340489ee8beb15f74f59b",
   },
 });
