4e01d9a33f897fb91c2e7eaf721d10314560fec7
Somesh Vemula <ratansomesh@gmail.com>
2023-11-15 19:14:29 -0500

    replace API key with environment variable

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -9,7 +9,7 @@ export interface FetchResponse<T> {
 const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "01021587a22340489ee8beb15f74f59b",
+    key: process.env.CLIENT_SECRET,
   },
 });
 
