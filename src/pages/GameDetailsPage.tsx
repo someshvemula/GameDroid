1711f4d3a7bfcb70a771bca95ddce8f0266e7cc3
Somesh Vemula <ratansomesh@gmail.com>
2023-11-17 16:59:55 -0500

    build expandable text

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,11 +1,12 @@
-import { Heading, Spinner, Text, Box } from "@chakra-ui/react";
-import React from "react";
+import { Heading, Spinner, Button, Collapse } from "@chakra-ui/react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 
 const GameDetailsPage = () => {
   const { slug } = useParams();
   const { data: game, error, isLoading } = useGame(slug!);
+  const [expanded, setExpanded] = useState(false);
 
   if (isLoading) return <Spinner></Spinner>;
 
@@ -13,7 +14,17 @@ const GameDetailsPage = () => {
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <Collapse startingHeight={24} animateOpacity={true} in={expanded}>
+        {game.description_raw}
+      </Collapse>
+      <Button
+        colorScheme={expanded ? "red" : "green"}
+        size="sm"
+        onClick={() => setExpanded(!expanded)}
+        mt="1rem"
+      >
+        Show {expanded ? "Less" : "More"}
+      </Button>
     </>
   );
 };
