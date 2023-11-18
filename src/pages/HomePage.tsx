371db64061eb3703bca82e3d3693b0b3158ab0a3
Somesh Vemula <ratansomesh@gmail.com>
2023-11-18 00:50:20 -0500

    Refactor- clear padding issue with GenreList

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -20,7 +20,7 @@ const HomePage = () => {
       }}
     >
       <Show above="lg">
-        <GridItem area="aside" paddingX={5}>
+        <GridItem area="aside">
           <GenreList />
         </GridItem>
       </Show>
