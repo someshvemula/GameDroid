0d55e6eb2dc1c03a86aaadee30dd1c436f1b02a0
Somesh Vemula <ratansomesh@gmail.com>
2023-11-22 20:03:56 -0500

    modify the iframe

--- src/components/YllixAd.tsx
+++ src/components/YllixAd.tsx
@@ -11,13 +11,14 @@ const YllixAd = () => {
       </Box>
     );
   }
+  var qs = Math.round(Math.random() * 10000000);
   const { pathname } = useLocation();
   const searchParams = useParams();
   useEffect(() => {
     const loadYllixScript = () => {
       const script = document.createElement("script");
       script.src = YllixData.adScriptSrc;
-      script.async = false;
+      script.async = true;
       document.head.appendChild(script);
     };
 
@@ -27,7 +28,7 @@ const YllixAd = () => {
   return (
     <div>
       {/* The ad will load inside this iframe */}
-      <iframe
+      {/* <iframe
         title="Yllix Ad"
         src={
           "https://udbaa.com/bnr.php?section=General&pub=437177&format=728x90&ga=g"
@@ -35,6 +36,17 @@ const YllixAd = () => {
         width="728"
         height="90"
         style={{ border: "none", margin: 0, padding: 0 }}
+      ></iframe> */}
+      <iframe
+        src={
+          "https://udbaa.com/bnr_xload.php?section=General&pub=437177&format=728x90&ga=g&xt=170070029721063&xtt=" +
+          qs
+        }
+        width="728"
+        height="90"
+        scrolling="no"
+        sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts"
+        style={{ width: "728px", height: "90px" }}
       ></iframe>
     </div>
   );
